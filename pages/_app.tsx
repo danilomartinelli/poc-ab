5ef30c20fe5baba31e3fb9871ecb7439eb228581
Danilo Martinelli <danilo.martinelli@rdstation.com>
2022-02-16 14:51:30 -0300

    configure firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAahxhmfiQPCuwJx8Tsy9Nt-HizB-VJRNU",
+  authDomain: "teste-ab-60e62.firebaseapp.com",
+  projectId: "teste-ab-60e62",
+  storageBucket: "teste-ab-60e62.appspot.com",
+  messagingSenderId: "183844350446",
+  appId: "1:183844350446:web:bb63803fab6086286806ff",
+  measurementId: "G-JNKY4K91X0"
+};
+
+if (typeof window !== "undefined") {
+  const app = initializeApp(firebaseConfig);
+  const _analytics = getAnalytics(app);
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
